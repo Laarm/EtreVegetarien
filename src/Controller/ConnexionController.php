a5b2aeca5abd0c524b2eaefc420b35fb282ae61e
Arnaud <vieprivee01@goweet.fr>
2019-11-12 10:50:05 +0100

    Reset

---
+++ src/Controller/ConnexionController.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ConnexionController extends AbstractController
+{
+    public function index()
+    {
+        return $this->render('connexion.html.twig', [
+            'user_first_name' => "test",
+        ]);
+    }
+}
