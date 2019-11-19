07e5917b69cbb2c78fa8bb21503e40d00f112a9e
Arnaud <vieprivee01@goweet.fr>
2019-11-19 11:52:30 +0100

    Mise en place du design de la page restaurants et magasins

--- src/Controller/MagasinController.php
+++ src/Controller/MagasinController.php
@@ -4,16 +4,19 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\MagasinRepository;
 
 class MagasinController extends AbstractController
 {
     /**
      * @Route("/magasin", name="magasin")
      */
-    public function index()
+    public function index(MagasinRepository $magasinrepo)
     {
+        $magasin = $magasinrepo->findAll();
         return $this->render('magasin/index.html.twig', [
             'controller_name' => 'MagasinController',
+            'magasins' => $magasin,
         ]);
     }
 }
--- src/Controller/RestaurantController.php
+++ src/Controller/RestaurantController.php
@@ -4,16 +4,19 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\RestaurantRepository;
 
 class RestaurantController extends AbstractController
 {
     /**
      * @Route("/restaurants", name="restaurants")
      */
-    public function index()
+    public function index(RestaurantRepository $restaurantrepo)
     {
+        $restaurant = $restaurantrepo->findAll();
         return $this->render('restaurant/index.html.twig', [
             'controller_name' => 'RestaurantController',
+            'restaurants' => $restaurant,
         ]);
     }
 }
