07afcf4ac3a4a2d5631aab561e0160be00ea61d6
Arnaud <vieprivee01@goweet.fr>
2019-12-13 09:57:39 +0100

    Possibilité de commenter

--- public/js/pagination.js
+++ public/js/pagination.js
@@ -16,48 +16,48 @@ class pagination {
         this.name = name
         this.displayMax = displayMax
         this.displayBtnMax = displayBtnMax
-        if(this.displayBtnMax==0){this.displayBtnMax = 0}
+        if (this.displayBtnMax == 0) { this.displayBtnMax = 0 }
         this.option = option
         this.displayCount = 0
         this.tested = ""
-        this.nombreObjet = $( '[paginationName="'+name+'"]' ).length
-        this.nombrePagination = this.nombreObjet/this.displayMax
+        this.nombreObjet = $('[paginationName="' + name + '"]').length
+        this.nombrePagination = this.nombreObjet / this.displayMax
         this.paginationInit()
     }
     paginationInit() {
-        if(this.nombreObjet>this.displayMax){
-            if(this.nombrePagination<this.displayBtnMax){
-                this.displayBtnMax = this.nombreObjet/this.displayMax
+        if (this.nombreObjet > this.displayMax) {
+            if (this.nombrePagination < this.displayBtnMax) {
+                this.displayBtnMax = this.nombreObjet / this.displayMax
             }
-            for (let display = 1; display < Math.ceil(this.displayBtnMax)+1; display++) {
-                if(display==1){this.varBonus="active"}else{this.varBonus=null}
-                $('[paginationBtn="'+this.name+'"] .btnPagination').append( '<li btnPaginationId="'+display+'" class="page-item cursor-pointer '+this.varBonus+'"><a class="page-link">'+display+'</a></li>' );
-                this.btnPagination = document.querySelector('[btnPaginationId="'+display+'"]');
+            for (let display = 1; display < Math.ceil(this.displayBtnMax) + 1; display++) {
+                if (display == 1) { this.varBonus = "active" } else { this.varBonus = null }
+                $('[paginationBtn="' + this.name + '"] .btnPagination').append('<li btnPaginationId="' + display + '" class="page-item cursor-pointer ' + this.varBonus + '"><a class="page-link">' + display + '</a></li>');
+                this.btnPagination = document.querySelector('[btnPaginationId="' + display + '"]');
                 this.lastBtnPagination = display
                 this.btn(this.btnPagination)
             }
-            $('[paginationName="'+this.name+'"]').css('display','none');
-            for (this.displayCount; this.displayCount < this.displayMax+1; this.displayCount++) {
-                $('[paginationId="'+this.displayCount+'"]').css('display',this.option);
+            $('[paginationName="' + this.name + '"]').css('display', 'none');
+            for (this.displayCount; this.displayCount < this.displayMax + 1; this.displayCount++) {
+                $('[paginationId="' + this.displayCount + '"]').css('display', this.option);
             }
         }
     }
     btn(btnPagination) {
         btnPagination.addEventListener("click", () => {
             var btnPaginationId = $(btnPagination).attr('btnPaginationId')
-            if(btnPaginationId==1){
+            if (btnPaginationId == 1) {
                 this.displayCount = 0;
-            } else if (btnPaginationId==2) {
+            } else if (btnPaginationId == 2) {
                 this.displayCount = this.displayMax;
-            } else if ((btnPaginationId*this.displayMax) > this.nombreObjet) {
-                this.displayCount = this.nombreObjet-1
+            } else if ((btnPaginationId * this.displayMax) > this.nombreObjet) {
+                this.displayCount = this.nombreObjet - 1
             } else {
-                this.displayCount = this.displayMax*this.displayMax;
+                this.displayCount = this.displayMax * this.displayMax;
             }
-            $('[paginationView="'+this.name+'"]').load("?view="+this.displayCount+'&maxView='+this.displayMax+' [paginationView="'+this.name+'"] .contentPagination', function() {
-                this.displayCount = this.displayCount+this.displayMax
-            $('[btnPaginationId').removeClass("active")
-            $('[btnPaginationId="'+btnPaginationId+'"').addClass("active")
+            $('[paginationView="' + this.name + '"]').load("?view=" + this.displayCount + '&maxView=' + this.displayMax + ' [paginationView="' + this.name + '"] .contentPagination', function () {
+                this.displayCount = this.displayCount + this.displayMax
+                $('[btnPaginationId').removeClass("active")
+                $('[btnPaginationId="' + btnPaginationId + '"').addClass("active")
             });
         })
     }
--- src/Controller/RestaurantController.php
+++ src/Controller/RestaurantController.php
@@ -10,6 +10,7 @@ use App\Repository\RestaurantAvisRepository;
 use App\Repository\ArticleRepository;
 use App\Entity\Restaurant;
 use App\Entity\RestaurantAvis;
+use Doctrine\ORM\EntityManager;
 use Symfony\Component\HttpFoundation\Response;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -33,14 +34,35 @@ class RestaurantController extends AbstractController
     /**
      * @Route("/restaurant/{id}", name="restaurant_show")
      */
-    public function showRestaurant(Restaurant $restaurant, ArticleRepository $repo, RestaurantRepository $repoRestaurant)
+    public function showRestaurant(Restaurant $restaurant, ArticleRepository $repo, RestaurantRepository $repoRestaurant, RestaurantAvisRepository $repoRestaurantAvis, Request $request)
     {
         $articles = $repo->findBy(array(), null, "5", null);
         $autresrestaurants = $repoRestaurant->findBy(array('ville' => $restaurant->getVille()), null, "10", null);
+        // $restaurantAvis = $this->getDoctrine()
+        //     ->getRepository(Restaurant::class)
+        //     ->find($restaurant->getId());
+        // $result = $em->getRepository(RestaurantAvis::class)->createQueryBuilder('r')
+        //     ->select('r')
+        //     ->where('r.restaurant_id = :restaurantId')
+        //     ->setParameter('restaurantId', $restaurant->getId())
+        //     ->orderBy('r.created_at', 'ASC')
+        //     ->getQuery();
+        // $restaurantAvis = $result->getResult();
+
+        if ($request->get('view') !== null) {
+            $view = $request->get('view');
+            $maxView = $request->get('maxView');
+        } else {
+            $view = null;
+            $maxView = 100;
+        }
+        $restaurantAvis = $repoRestaurantAvis->findBy(array('restaurant' => $restaurant), null, $maxView, $view);
+
         return $this->render('restaurant/restaurant.html.twig', [
             'restaurant' => $restaurant,
             'articles' => $articles,
             'autresrestaurants' => $autresrestaurants,
+            'restaurantAvisAll' => $restaurantAvis,
         ]);
     }
 
@@ -74,30 +96,46 @@ class RestaurantController extends AbstractController
     /**
      * @Route("/ajax/restaurant/sendNote", name="send_note_restaurant")
      */
-    public function sendNote(ValidatorInterface $validator, EntityManagerInterface $entityManager, Security $security): Response
+    public function sendNote(ValidatorInterface $validator, EntityManagerInterface $entityManager, Security $security, Request $request, RestaurantAvisRepository $repoRestaurantAvis): Response
     {
-        $note = htmlspecialchars($_POST['note']);
-        $restaurantId = htmlspecialchars($_POST['restaurant_id']);
-        if (!empty($note) && !empty($restaurantId)) {
-            if (empty($message)) {
-                $message = null;
-            } else {
-                $message = htmlspecialchars($_POST['message']);
-            }
-            $user = $security->getUser();
-            $sqlRestaurantAvis = new RestaurantAvis();
-            $sqlRestaurantAvis->setRestaurant($restaurantId)
-                ->setPostedBy($user->getId())
-                ->setMessage($message)
-                ->setNote($note)
-                ->setCreatedAt(new \DateTime());
-            $entityManager->persist($sqlRestaurantAvis);
-            $entityManager->flush();
-            $errors = $validator->validate($sqlRestaurantAvis);
-            if (count($errors) == 0) {
-                return $this->json(['code' => 200, 'message' => 'Merci d\'avoir donné votre avis !'], 200);
+        if ($request->isXmlHttpRequest()) {
+            $submittedToken = $request->request->get('csrfData');
+            if ($this->isCsrfTokenValid('send-note', $submittedToken)) {
+                $note = $request->request->get('note');
+                if (!empty($note) && !empty($request->request->get('restaurant_id'))) {
+                    if ($request->request->get('message') == "") {
+                        $message = null;
+                    } else {
+                        $message = $request->request->get('message');
+                    }
+                    $user = $security->getUser();
+                    $verif = $this->getDoctrine()
+                        ->getRepository(RestaurantAvis::class)
+                        ->findOneBy([
+                            'postedBy' => $user,
+                        ]);
+                    if (!$verif) {
+                        $sqlRestaurantAvis = new RestaurantAvis();
+                        $restaurant = $this->getDoctrine()
+                            ->getRepository(Restaurant::class)
+                            ->find($request->request->get('restaurant_id'));
+                        $sqlRestaurantAvis->setRestaurant($restaurant)
+                            ->setPostedBy($user)
+                            ->setMessage($message)
+                            ->setNote($note)
+                            ->setCreatedAt(new \DateTime());
+                        $entityManager->persist($sqlRestaurantAvis);
+                        $entityManager->flush();
+                        $errors = $validator->validate($sqlRestaurantAvis);
+                        if (count($errors) == 0) {
+                            return $this->json(['code' => 200, 'message' => 'Merci d\'avoir donné votre avis !'], 200);
+                        }
+                    } else {
+                        return $this->json(['code' => 400, 'message' => 'Vous avez déjà donné votre avis !'], 200);
+                    }
+                }
+                return $this->json(['code' => 400, 'message' => 'Veuillez contacter un administrateur !'], 200);
             }
         }
-        return $this->json(['code' => 400, 'message' => 'Veuillez contacter un administrateur !'], 200);
     }
 }
