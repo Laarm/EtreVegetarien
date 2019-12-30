11c569d63e13ca89f78fd76432d5f5464e061e23
Arnaud <vieprivee01@goweet.fr>
2019-12-30 14:44:35 +0100

    Correction que conseille codeclimate

--- src/Controller/InscriptionController.php
+++ src/Controller/InscriptionController.php
@@ -26,71 +26,31 @@ class InscriptionController extends AbstractController
     }
     public function inscriptionAction(ValidatorInterface $validator, UserPasswordEncoderInterface $passwordEncoder, UserRepository $repo, Request $request): Response
     {
-        if ($request->isXmlHttpRequest()) {
-            $submittedToken = $request->get('csrfData');
-            if ($this->isCsrfTokenValid('create-user-inscription', $submittedToken)) {
-                if (
-                    strlen($request->get('username')) > 5
-                    &&
-                    strlen($request->get('username')) < 20
-                    &&
-                    strlen($request->get('email')) > 5
-                    &&
-                    strlen($request->get('email')) < 70
-                    &&
-                    preg_match('#^(([a-z0-9!\#$%&\\\'*+/=?^_`{|}~-]+\.?)*[a-z0-9!\#$%&\\\'*+/=?^_`{|}~-]+)@(([a-z0-9-_]+\.?)*[a-z0-9-_]+)\.[a-z]{2,}$#i', $request->get('email'))
-                    &&
-                    strlen($request->get('password')) > 6
-                    &&
-                    strlen($request->get('password')) < 70
-                ) {
-                    $username = htmlspecialchars($request->get('username'));
-                    $email = htmlspecialchars($request->get('email'));
-                    $user = $repo->findOneBy([
-                        'email' => $email,
-                    ]);
-                    if ($user == null) {
-                        $sqlUser = new User();
-                        $password = $passwordEncoder->encodePassword($sqlUser, $request->get('password'));
-                        $sqlUser->setUsername($username)
-                            ->setPassword($password)
-                            ->setEmail($email)
-                            ->setCreatedAt(new \DateTime());
-                        $errors = $validator->validate($sqlUser);
-                        if (count($errors) == 0) {
-                            $sqlUser = $this->getDoctrine()->getRepository(User::class)->createUser($sqlUser);
-                            return $this->json(['message' => 'Vous êtes bien inscrit !'], 200);
-                        }
-                        return $this->json(['message' => 'Problème au niveau du serveur, veuillez contacter un administrateur !'], 400);
-                    } else {
-                        return $this->json(['message' => 'L\'email est déjà utilisé !'], 400);
+        if ($this->isCsrfTokenValid('create-user-inscription', $request->get('csrfData'))) {
+            if (strlen($request->get('username')) > 5 && strlen($request->get('username')) < 20
+                && strlen($request->get('email')) > 5 && strlen($request->get('email')) < 70
+                && preg_match('#^(([a-z0-9!\#$%&\\\'*+/=?^_`{|}~-]+\.?)*[a-z0-9!\#$%&\\\'*+/=?^_`{|}~-]+)@(([a-z0-9-_]+\.?)*[a-z0-9-_]+)\.[a-z]{2,}$#i', $request->get('email'))
+                && strlen($request->get('password')) > 6 && strlen($request->get('password')) < 70) {
+                if (!$repo->findOneBy(['email' => $request->get('email')])) {
+                    $sqlUser = new User();
+                    $sqlUser->setUsername($request->get('username'))->setPassword($passwordEncoder->encodePassword($sqlUser, $request->get('password')))->setEmail($request->get('email'))->setCreatedAt(new \DateTime());
+                    if (count($validator->validate($sqlUser)) == 0) {
+                        $this->getDoctrine()->getRepository(User::class)->createUser($sqlUser);
+                        return $this->json(['message' => 'Vous êtes bien inscrit !'], 200);
                     }
-                } else {
-                    if (strlen($request->get('username')) < 5) {
-                        $message = "Votre pseudonyme est trop court !";
-                    }
-                    if (strlen($request->get('username')) > 20) {
-                        $message = "Votre pseudonyme est trop long !";
-                    }
-                    if (strlen($request->get('email')) < 5) {
-                        $message = "Votre email est trop court !";
-                    }
-                    if (strlen($request->get('email')) > 70) {
-                        $message = "Votre email est trop long !";
-                    }
-                    if (strlen($request->get('password')) < 6) {
-                        $message = "Votre mot de passe est trop court !";
-                    }
-                    if (strlen($request->get('password')) > 70) {
-                        $message = "Votre mot de passe est trop long !";
-                    }
-                    if (!preg_match('#^(([a-z0-9!\#$%&\\\'*+/=?^_`{|}~-]+\.?)*[a-z0-9!\#$%&\\\'*+/=?^_`{|}~-]+)@(([a-z0-9-_]+\.?)*[a-z0-9-_]+)\.[a-z]{2,}$#i', $request->get('email'))) {
-                        $message = "Votre email n'est pas conforme (exemple : exemple@exemple.fr) !";
-                    }
-                    if($message){return $this->json(['message' => $message], 400);}
-                    return $this->json(['message' => 'Veuillez remplir tout les champs !'], 400);
+                    return $this->json(['message' => 'Problème au niveau du serveur, veuillez contacter un administrateur !'], 400);
                 }
+                return $this->json(['message' => 'L\'email est déjà utilisé !'], 400);
             }
+            if (strlen($request->get('username')) < 5) {$message = "Votre pseudonyme est trop court !";}
+            if (strlen($request->get('username')) > 20) {$message = "Votre pseudonyme est trop long !";}
+            if (strlen($request->get('email')) < 5) {$message = "Votre email est trop court !";}
+            if (strlen($request->get('email')) > 70) {$message = "Votre email est trop long !";}
+            if (strlen($request->get('password')) < 6) {$message = "Votre mot de passe est trop court !";}
+            if (strlen($request->get('password')) > 70) {$message = "Votre mot de passe est trop long !";}
+            if (!preg_match('#^(([a-z0-9!\#$%&\\\'*+/=?^_`{|}~-]+\.?)*[a-z0-9!\#$%&\\\'*+/=?^_`{|}~-]+)@(([a-z0-9-_]+\.?)*[a-z0-9-_]+)\.[a-z]{2,}$#i', $request->get('email'))) {$message = "Votre email n'est pas conforme (exemple : exemple@exemple.fr) !";}
+            if($message){return $this->json(['message' => $message], 400);}
+            return $this->json(['message' => 'Veuillez remplir tout les champs !'], 400);
         }
     }
 }
