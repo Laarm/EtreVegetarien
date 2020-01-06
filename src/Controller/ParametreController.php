9a6320c576e4d4f9887baef8d67bc2cc511e530b
Arnaud <vieprivee01@goweet.fr>
2020-01-06 17:55:55 +0100

    Optimisation du uploadImage

--- src/Controller/ParametreController.php
+++ src/Controller/ParametreController.php
@@ -38,28 +38,25 @@ class ParametreController extends AbstractController
      */
     public function uploadImage(Request $request, Filesystem $filesystem, ValidatorInterface $validator): Response
     {
-        if ($request->isXmlHttpRequest()) {
-            $submittedToken = $request->get('_token');
-            if ($this->isCsrfTokenValid('upload-avatar', $submittedToken)) {
-                $uploadedFile = $request->files->get('file');
-                $imageConstraint = new Assert\Image([
-                    "maxSize" => "1m"
-                ]);
-                if ($validator->validate($uploadedFile, [$imageConstraint])->count() > 0) {
-                    return $this->json(['message' => $validator->validate($uploadedFile, [$imageConstraint])], 400);
-                }
-                $filename = uniqid("", true) . $uploadedFile->getClientOriginalName();
-                $uploadedFile->move(__DIR__ . '/../../public/img/uploads/avatars', $filename);
-                $oldImage = $this->getDoctrine()->getRepository(User::class)->find($this->getUser()->getId());
-                if (!empty($oldImage->getAvatar()) && substr($oldImage->getAvatar(), 0, 4) !== "http" && 'img/uploads/avatars/' . $filename !== $oldImage->getAvatar()) {
-                    $filesystem->remove(['symlink', "../public/" . $oldImage->getAvatar(), 'activity.log']);
-                }
-                $oldImage->setAvatar('img/uploads/avatars/' . $filename);
-                $this->getDoctrine()->getRepository(User::class)->saveUserAvatar($oldImage);
-                return $this->json(['message' => 'Vous avez bien envoyer l\'image !', 'location' => 'img/uploads/avatars/' . $filename], 200);
+        if ($this->isCsrfTokenValid('upload-avatar', $request->get('_token'))) {
+            $uploadedFile = $request->files->get('file');
+            $imageConstraint = new Assert\Image([
+                "maxSize" => "1m"
+            ]);
+            if ($validator->validate($uploadedFile, [$imageConstraint])->count() > 0) {
+                return $this->json(['message' => $validator->validate($uploadedFile, [$imageConstraint])], 400);
             }
-            return $this->json(['message' => 'Erreur'], 400);
+            $filename = uniqid("", true) . $uploadedFile->getClientOriginalName();
+            $uploadedFile->move(__DIR__ . '/../../public/img/uploads/avatars', $filename);
+            $oldImage = $this->getDoctrine()->getRepository(User::class)->find($this->getUser()->getId());
+            if (!empty($oldImage->getAvatar()) && 'img/uploads/avatars/' . $filename !== $oldImage->getAvatar()) {
+                $filesystem->remove(['symlink', "../public/" . $oldImage->getAvatar(), 'activity.log']);
+            }
+            $oldImage->setAvatar('img/uploads/avatars/' . $filename);
+            $this->getDoctrine()->getRepository(User::class)->saveUserAvatar($oldImage);
+            return $this->json(['message' => 'Vous avez bien envoyer l\'image !', 'location' => 'img/uploads/avatars/' . $filename], 200);
         }
+        return $this->json(['message' => 'Erreur'], 400);
     }
 
     /**
@@ -71,24 +68,19 @@ class ParametreController extends AbstractController
      */
     public function saveProfil(Request $request, ValidatorInterface $validator): Response
     {
-        if ($request->isXmlHttpRequest()) {
-            $submittedToken = $request->get('csrfData');
-            if ($this->isCsrfTokenValid('save-profil', $submittedToken)) {
-                $sqlUser = $this->getDoctrine()->getRepository(User::class)->find($this->getUser()->getId());
-                $sqlUser->setUsername($request->get('username'))
-                    ->setEmail($request->get('email'))
-                    ->setPreference($request->get('preference'))
-                    ->setPreferenceCreatedAt(new \DateTime($request->get('date')))
-                    ->setBio($request->get('bio'));
-                $errors = $validator->validate($sqlUser);
-                if (count($errors) == 0) {
-                    $this->getDoctrine()->getRepository(User::class)->saveUserProfil($sqlUser);
-                    return $this->json(['message' => 'Votre profil à bien été sauvegarder !'], 200);
-                }
-                return $this->json(['message' => 'Erreur, veuillez contacter un administrateur !'], 400);
+        if ($this->isCsrfTokenValid('save-profil', $request->get('csrfData'))) {
+            $sqlUser = $this->getDoctrine()->getRepository(User::class)->find($this->getUser()->getId());
+            $sqlUser->setUsername($request->get('username'))
+                ->setEmail($request->get('email'))
+                ->setPreference($request->get('preference'))
+                ->setPreferenceCreatedAt(new \DateTime($request->get('date')))
+                ->setBio($request->get('bio'));
+            if (count($validator->validate($sqlUser)) == 0) {
+                $this->getDoctrine()->getRepository(User::class)->saveUserProfil($sqlUser);
+                return $this->json(['message' => 'Votre profil à bien été sauvegarder !'], 200);
             }
-            return $this->json(['message' => 'Erreur'], 400);
         }
+        return $this->json(['message' => 'Erreur, veuillez contacter un administrateur !'], 400);
     }
 
     /**
@@ -123,21 +115,16 @@ class ParametreController extends AbstractController
      */
     public function saveRS(Request $request, ValidatorInterface $validator): Response
     {
-        if ($request->isXmlHttpRequest()) {
-            $submittedToken = $request->get('csrfData');
-            if ($this->isCsrfTokenValid('save-rs', $submittedToken)) {
-                $sqlUser = $this->getDoctrine()->getRepository(User::class)->find($this->getUser()->getId());
-                $sqlUser->setFacebook($request->get('facebook'))
-                    ->setInstagram($request->get('instagram'))
-                    ->setYoutube($request->get('youtube'));
-                $errors = $validator->validate($sqlUser);
-                if (count($errors) == 0) {
-                    $this->getDoctrine()->getRepository(User::class)->saveUserProfil($sqlUser);
-                    return $this->json(['message' => 'Vos réseaux sociaux ont bien été sauvegarder !'], 200);
-                }
-                return $this->json(['message' => 'Erreur, veuillez contacter un administrateur !'], 400);
+        if ($this->isCsrfTokenValid('save-rs', $request->get('csrfData'))) {
+            $sqlUser = $this->getDoctrine()->getRepository(User::class)->find($this->getUser()->getId());
+            $sqlUser->setFacebook($request->get('facebook'))
+                ->setInstagram($request->get('instagram'))
+                ->setYoutube($request->get('youtube'));
+            if (count($validator->validate($sqlUser)) == 0) {
+                $this->getDoctrine()->getRepository(User::class)->saveUserProfil($sqlUser);
+                return $this->json(['message' => 'Vos réseaux sociaux ont bien été sauvegarder !'], 200);
             }
-            return $this->json(['message' => 'Erreur'], 400);
         }
+        return $this->json(['message' => 'Erreur, veuillez contacter un administrateur'], 400);
     }
 }
