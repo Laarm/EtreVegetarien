94ed3cd3a501362d397c538f3d05c0d75eb18680
Arnaud <vieprivee01@goweet.fr>
2020-02-17 12:29:41 +0100

    Changement du Token de L'api

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -12,7 +12,7 @@ class IndexController extends AbstractController
     {
         $articles = $repo->findBy(array(), array('id' => 'DESC'), "4", null);
         $client = HttpClient::create();
-        $response = $client->request('GET', 'https://api.instagram.com/v1/users/self/media/recent/?access_token=5754011249.aed5496.f04a76880baa4a93941645c151dcf531');
+        $response = $client->request('GET', 'https://api.instagram.com/v1/users/self/media/recent/?access_token=5754011249.aed5496.9e69000c4ccc4d7b96349a5c1db4dd5f');
         return $this->render('index.html.twig', [
             'postInstagram' => json_decode($response->getContent(), true),
             'articles' => $articles,
